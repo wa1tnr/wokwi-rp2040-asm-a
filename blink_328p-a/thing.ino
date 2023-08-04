0cea02b18e2e61f9ac57984735a35b31e2657edc
wa1tnr <wa1tnr@users.noreply.github.com>
2023-08-04 07:15:43 +0000

    sync to wokwi version

--- blink_328p-a/thing.ino
+++ blink_328p-a/thing.ino
@@ -1,1 +1,16 @@
-Fri  4 Aug 07:11:59 UTC 2023
+/* wa1tnr 04 Aug 2023 06:15z */
+
+/* empty .ino file: */
+
+/* https://www.gammon.com.au/forum/?id=12625 */
+
+/*
+
+How to avoid this problem?
+
+
+The solution is simple: Leave your main .ino file empty! Yes, that's it.
+
+*/
+
+/* end. */
