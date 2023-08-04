9e83f0c3359722d117cdb22b41bc5c78a58ea555
wa1tnr <wa1tnr@users.noreply.github.com>
2023-08-04 07:16:55 +0000

    sync to wokwi version
    likely the last edit for a complete sync.

--- blink_328p-a/things.cpp
+++ blink_328p-a/things.cpp
@@ -1,1 +1,32 @@
-Fri  4 Aug 07:12:08 UTC 2023
+/* wa1tnr 04 Aug 2023 06:15z */
+
+#include <Arduino.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+   void start();
+   void xpMain();
+#ifdef __cplusplus
+}
+#endif
+
+void setup_serial(void) {
+  Serial.begin(9600);
+}
+
+void setup(void) {
+  setup_serial();
+  Serial.print(" IN setup();\n");
+  Serial.print("  start();  ");
+  start();
+  delay(1000);
+  Serial.print("  xpMain();  ");
+}
+
+void loop(void) {
+  xpMain();
+  Serial.write('.');
+}
+
+// end.
